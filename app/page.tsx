d2678056f3fe7ecbeb19db94cc364b351d25e20d
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-18 07:24:15 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Get it Now',
+      label: 'Get it (Click for load NFT images)',
 
 
     },
