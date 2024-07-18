49099131d327e6dfa04668934cbe87074ba714ff
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-19 00:34:31 +0700

    update

--- app/api/frame-5/route.ts
+++ app/api/frame-5/route.ts
@@ -49,7 +49,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           },
           {
             label: `Back`,
-            target: `${NEXT_PUBLIC_URL}/api/frame`,
+            target: `${NEXT_PUBLIC_URL}/api/frame-2`,
            
           },
       ],
