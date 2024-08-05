f344aeed6812b3c84e3154abbbd942d6281b5b58
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-08-05 18:47:45 +0700

    update

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'https://newairdrop-list.vercel.app' : 'https://newairdrop-list.vercel.app';
+  process.env.NODE_ENV == 'development' ? 'https://10summer-nft.vercel.app' : 'https://10summer-nft.vercel.app';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
